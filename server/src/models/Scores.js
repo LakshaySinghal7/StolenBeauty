3c0670853b2e0f4e0410accbfd943b9d90a288c0
LakshaySinghal7 <lakshaysinghal2014@gmail.com>
2024-01-28 14:59:23 +0530

    ok

--- server/src/models/Scores.js
+++ server/src/models/Scores.js
@@ -4,7 +4,7 @@ const ScoreSchema = new mongoose.Schema({
   email: {
     type: String,
     lowercase: true,
-    required: [true, "can't be blank"],
+    required: [true, "Can't be blank"],
     match: [/\S+@\S+\.\S+/, "is invalid"],
     index: true,
   },
